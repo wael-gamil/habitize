c130729c99f38ec2df9dca929487f0ac8f163d61
Wael <131612777+ggwael8@users.noreply.github.com>
2025-04-11 18:56:12 +0200

    initialize habitize

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Orbitron, Inter } from 'next/font/google';
+
+export const orbitron = Orbitron({
+  variable: '--font-orbitron',
+  subsets: ['latin'],
+});
+
+export const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+});
